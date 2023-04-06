0fc0d7af9634f3b558d9299cf6d90af3ac89e9cc
sharmtus <tushar.sharma@effem.com>
2023-04-06 09:30:57 +0530

    created feeditem model

---
+++ EssentialLeeds/EssentialLeeds/EssentialLeeds.h
@@ -0,0 +1,18 @@
+//
+//  EssentialLeeds.h
+//  EssentialLeeds
+//
+//  Created by TUSHAR SHARMA on 06/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialLeeds.
+FOUNDATION_EXPORT double EssentialLeedsVersionNumber;
+
+//! Project version string for EssentialLeeds.
+FOUNDATION_EXPORT const unsigned char EssentialLeedsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialLeeds/PublicHeader.h>
+
+
---
+++ EssentialLeeds/EssentialLeeds/FeedItems.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItems.swift
+//  EssentialLeeds
+//
+//  Created by TUSHAR SHARMA on 06/04/23.
+//
+
+import Foundation
+
+struct FeedItems{
+    let id:UUID
+    let description:String?
+    let location:String?
+    let imageUrl:URL
+}
